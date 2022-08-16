ae702c082df82781477d1deeaef1d301b6772bf0
Edu <eduardobrunner94@gmil.com>
2022-08-16 19:25:28 -0300

    mejoras de datos FastAPI() en Swagger

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from routes.user import user
+from docs import tags_metadata
 
-app = FastAPI() #me devuelve un objeto app
+app = FastAPI(
+    title="MI APLICAION CON FastAPI y Mongodb",
+    description="esta es una pequeña api de prueba",
+    version="0.0.1",
+    openapi_tags=tags_metadata
+) #me devuelve un objeto app
 
 app.include_router(user)
